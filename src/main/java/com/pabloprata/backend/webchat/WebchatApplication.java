33031e2e5d7e99fad0b2092dd38b71cc6e93b64a
Pablo Prata <pabloalmeidaprata@hotmail.com>
2025-01-29 18:15:11 -0300

    Spring Init

--- src/main/java/com/pabloprata/backend/webchat/WebchatApplication.java
+++ src/main/java/com/pabloprata/backend/webchat/WebchatApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WebchatApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(WebchatApplication.class, args);
 	}
+
 }
